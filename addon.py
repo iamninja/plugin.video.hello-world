0d0635b6a1efaa543bcc89569cb5bd5373dbc5f9
Vagios Vlachos <bagios234@gmail.com>
2015-12-04 04:34:37 +0200

    Create directory like view

--- addon.py
+++ addon.py
@@ -1,13 +1,34 @@
 import sys
+import urllib
+import urlparse
 import xbmcgui
 import xbmcplugin
 
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:]) #Skip initial ?
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = "http://localhost/some_video.mkv"
-li = xbmcgui.ListItem('My first video!', iconImage="DefaultVideo.png")
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def build_url(query):
+	return base_url + '?' + urllib.urlencode(query)
 
-xbmcplugin.endOfDirectory(addon_handle)
+mode = args.get('mode', None)
+
+if mode is None:
+	url = build_url({'mode': 'folder', 'foldername': 'Folder One'})
+	li = xbmcgui.ListItem('Folder One', iconImage="DefaultFolder.png")
+	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+	url = build_url({'mode': 'folder', 'foldername': 'Folder Two'})
+	li = xbmcgui.ListItem('Folder Two', iconImage="DefaultFolder.png")
+	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+	xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'folder':
+	foldername = args['foldername'][0]
+	url = 'http://localhost/a-video.mkv'
+	li = xbmcgui.ListItem(foldername + ' Video', iconImage="DefaultVideo.png")
+	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+	xbmcplugin.endOfDirectory(addon_handle)
